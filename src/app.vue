1adc7bef1565ab8c20c88f83178471bdf6152bb4
shiloox <ada44lovelace@gmail.com>
2025-10-06 19:35:48 -0500

    chore:inital commit

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<script setup>
+import UserLogin from "./hotel/presentation/view/user-login.vue";
+</script>
+
+<template>
+  <div>
+    <h1>cachudo de mrd</h1>
+
+  </div>
+  <user-login />
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './app.vue'
+import PrimeVue from 'primevue/config';
+import Material from "@primeuix/themes/material";
+import i18n from "./i18n.js";
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css'
+import router from "./router.js";
+import pinia from "pinia";
+import {
+    Button,
+    Card,
+    ConfirmationService,
+    DialogService,
+    FloatLabel,
+    InputText,
+    Password, Toast,
+    ToastService, Toolbar
+} from "primevue";
+
+// noinspection JSCheckFunctionSignatures
+
+createApp(App)
+    .use(i18n)
+    .use(PrimeVue, { theme: { preset: Material}, ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-input-text', InputText)
+    .component('pv-password', Password)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-toast', Toast)
+    .component('pv-toolbar', Toolbar)
+    .use(router)
+    .use(pinia)
+    .mount('#app')
